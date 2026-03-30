aafa88fb9676ff3c3eb4f20e85dbb714126da03f
yathy1040 <tyathusan26@gmail.com>
2026-03-29 23:15:18 -0400

    Create file framework for project, start changing initial values for the project.

--- app/page.tsx
+++ app/page.tsx
@@ -14,10 +14,10 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+            Job Hunt Command Center
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
+            Manage applications, interviews, contacts, and next steps in one place.{" "}
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
